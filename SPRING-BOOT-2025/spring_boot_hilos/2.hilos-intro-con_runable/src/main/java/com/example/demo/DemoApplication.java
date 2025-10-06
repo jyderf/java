56387a8800a2f87b6df761612c8d351b7ac1d3db
jyderf <fredy.ingenieria@gmail.com>
2025-10-06 16:12:06 -0500

    finalización de hilos 1 intro

---
+++ SPRING-BOOT-2025/spring_boot_hilos/2.hilos-intro-con_runable/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+        Proceso_1 a = new Proceso_1();
+        Proceso_2 b = new Proceso_2();
+        a.start();
+        b.start();
+	}
+
+}
